22ce86177e9ca5029ea0ad0e54e315a5342be68c
somename <someemail>
2015-10-21 15:02:09 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqat2015/planetExplorer/PlanetExplorer.java
+++ src/org/unioulu/tol/sqat2015/planetExplorer/PlanetExplorer.java
@@ -25,6 +25,39 @@ public class PlanetExplorer {
 		 * The return string should also contain a list of coordinates of the encountered obstacles. No white spaces.
 		 */
 		
-		return null;
+		int pos_x=0;
+		int pos_y=0;
+		char facing=78;
+		String location = pos_x + "," + pos_y + "," + facing;
+		
+		for (int index = 0; index < command.length();index++) {
+			char c = command.charAt(index);
+			if (c==102) { //forward
+				if (facing == 78) pos_x++; //NORTH
+				else if (facing == 83) pos_x--; //SOUTH
+				else if (facing == 69) pos_y++; //EAST
+				else if (facing == 87) pos_y--; //WEST
+			}
+			else if (c==98) { //backward
+				if (facing == 78) pos_x--; //NORTH
+				else if (facing == 83) pos_x++; //SOUTH
+				else if (facing == 69) pos_y--; //EAST
+				else if (facing == 87) pos_y++; //WEST
+			}
+			else if (c==114) { //right
+				if (facing == 78) facing = 69; //NORTH -> EAST
+				else if (facing == 83) facing = 87; //SOUTH -> WEST
+				else if (facing == 69) facing = 83; //EAST -> SOUTH
+				else if (facing == 87) facing = 78; //WEST -> NORTH
+			}
+			else if (c==114) { //right
+				if (facing == 78) facing = 87; //NORTH -> WEST
+				else if (facing == 83) facing = 69; //SOUTH -> EAST
+				else if (facing == 69) facing = 78; //EAST -> NORTH
+				else if (facing == 87) facing = 83; //WEST -> SOUTH
+			}
+		}
+		
+		return location;
 	}
 }
