d0eb4443d382cc8116e5f22edcb2fd9ae72d76a4
somename <someemail>
2015-10-21 16:26:29 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqat2015/planetExplorer/PlanetExplorer.java
+++ src/org/unioulu/tol/sqat2015/planetExplorer/PlanetExplorer.java
@@ -101,6 +101,7 @@ public class PlanetExplorer {
 				for (int k = 0; k < obstacleX.length;k++) {
 					if (pos_x == obstacleX[k] && pos_y == obstacleY[k]) collision=1;
 				}
+				k=0;
 			}
 			if (pos_x>=0 && pos_x<=x && pos_y>=0 && pos_y<=y & collision == 0) location = pos_x + "," + pos_y + "," + facing;
 			collision = 0;
